1edb60b7fa571d851b60a43fb15f78795ae99d6b
Isaac Souza <isaacsms@id.uff.br>
2020-08-26 23:35:35 -0300

    firebase intro agora guarda dados de um aplicativo que consegue fazer upload de imagens na firestorage

--- firebase_intro/lib/main.dart
+++ firebase_intro/lib/main.dart
@@ -1,157 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
 void main() async {
-  runApp(MyApp());
-   Firestore db;
-    /**
-     * Salvar e atualizar dados no firebase é mole d++
-     * primeiro, criamos uma instancia do BD.
-     * Após isso, acessamos a coleção onde o dado ficará salvo
-     * dizemos qual é a chave do dado(ou seja, o que colocaremos
-     * numa função de busca pra retornar o dado)
-     * e depois, com o método setData, passamos
-     * um objeto do tipo Map para salvar as informações.
-     */
-    db = Firestore.instance;
-    db.collection("users").document("001").setData({
-      "nome": "isaac",
-      "idade": "20",
-    });
-
-    /**
-     * Porém, existe um problema grave ali em cima, você ta tendo que
-     * definir o ID(chave) previamente. num cenário real, isso é impossivel
-     * então ao invés de manualmente digitar a chave, usaremos o método
-     * add, que faz a mesma coisa que setData, mas ele gera
-     * uma chave única automatica para o nosso banco.
-     * 
-     * Mas e se a gente quiser editar esse valor???
-     * 
-     * Existem maneiras de se achar esse dado mesmo sem saber
-     * préviamente sua chave, um método é utilizando 
-     * o DocumentReference. já que o método add retorna justamente
-     * o id gerado, nós guardamos ele numa variavel do tipo document.
-     */
-
-    DocumentReference ref = await db.collection("users").add({
-      "nome": "chave automatica",
-      "idade": "999",
-    });
-    print(ref.documentID);
-  }
-
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-
- 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  runApp(
+    MaterialApp(
+      home: Home(),
+    ),
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  File _image;
+  String _statusUpload = "Upload não iniciado";
 
-  void _incrementCounter() {
+  Future _getImage(String imageSource) async {
+    if (imageSource == "camera") {
+      _image = await ImagePicker.pickImage(source: ImageSource.camera);
+    } else {
+      _image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _image = _image;
     });
+
+    
+  }
+
+  Future _uploadImage(File _image) async {
+    
+    FirebaseStorage storage = FirebaseStorage.instance;
+    StorageReference storageRoot = storage.ref();
+    StorageReference file = storageRoot.child("photos").child("photo2.jpg");
+
+    file.putFile(_image);
+  }
+
+  dynamic setImage(File _image) {
+    if (_image == null) return Container();
+    return Image.file(_image);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        appBar: AppBar(title: Text("Selecionando Imagens")),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              RaisedButton(
+                onPressed: () {
+                  _getImage("camera");
+                },
+                child: Text("Camera"),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _getImage("gallery");
+                },
+                child: Text("Galeria"),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _uploadImage(_image);
+                },
+                child: Text("Fazer upload da imagem."),
+              ),
+              setImage(_image),
+            ],
+          ),
+        ));
   }
 }
